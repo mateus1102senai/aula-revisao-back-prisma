ea0f34b1deed59970028052653d154a4cc998d18
Mateusm <mateus444666@gmail.com>
2025-04-01 10:53:47 -0300

    update project to use prisma on

--- prisma/migrations/20250401134804_alter_to_sqlite/migration.sql
+++ prisma/migrations/20250401134804_alter_to_sqlite/migration.sql
@@ -1,16 +1,14 @@
 -- CreateTable
 CREATE TABLE "animes" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "episodes" INTEGER NOT NULL,
     "releaseYear" INTEGER NOT NULL,
     "studio" TEXT NOT NULL,
     "genres" TEXT NOT NULL,
-    "rating" DOUBLE PRECISION NOT NULL,
+    "rating" REAL NOT NULL,
     "imageUrl" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "animes_pkey" PRIMARY KEY ("id")
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
 );
--- src/routes/animeRoutes.js
+++ src/routes/animeRoutes.js
@@ -1,22 +1,22 @@
 import express from "express";
 import AnimeController from "../controllers/animeController.js";
 
-const router = express.Router();
+const animeRouter = express.Router();
 
 // Rotas de Animes
 // GET /api/animes - Listar todos os animes
-router.get("/", AnimeController.getAllAnimes);
+animeRouter.get("/", AnimeController.getAllAnimes);
 
 // GET /api/animes/:id - Obter um anime pelo ID
-router.get("/:id", AnimeController.getAnimeById);
+animeRouter.get("/:id", AnimeController.getAnimeById);
 
 // POST /api/animes - Criar um novo anime
-router.post("/", AnimeController.createAnime);
+animeRouter.post("/", AnimeController.createAnime);
 
 // PUT /api/animes/:id - Atualizar um anime
-router.put("/:id", AnimeController.updateAnime);
+animeRouter.put("/:id", AnimeController.updateAnime);
 
 // DELETE /api/animes/:id - Remover um anime
-router.delete("/:id", AnimeController.deleteAnime);
+animeRouter.delete("/:id", AnimeController.deleteAnime);
 
-export default router;
+export default animeRouter;
